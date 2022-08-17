2d05ae56c2d52b0d0647995556122f7e47a13294
Saalim Quadri <danascape@gmail.com>
2022-08-17 08:45:19 +0530

    X00P: Switch to our fork

--- build_X00P-10.sh
+++ build_X00P-10.sh
@@ -17,11 +17,11 @@ git clone -b android-10 https://github.com/aosp-stuff/platform_hardware_qcom-caf
 git clone -b android-10 https://github.com/aosp-stuff/platform_hardware_qcom-caf_msm8996_display hardware/qcom-caf/msm8996/display
 git clone -b android-10 https://github.com/aosp-stuff/platform_hardware_qcom-caf_msm8996_media hardware/qcom-caf/msm8996/media
 
-git clone -b lineage-17.1 https://github.com/LineageOS/android_vendor_qcom_opensource_interfaces vendor/qcom/opensource/interfaces
+git clone -b android-10 https://github.com/aosp-stuff/platform_vendor_qcom_opensource_interfaces vendor/qcom/opensource/interfaces
 git clone -b lineage-17.1 https://github.com/LineageOS/android_vendor_codeaurora_telephony vendor/codeaurora/telephony
-git clone -b lineage-17.1 https://github.com/LineageOS/android_vendor_qcom_opensource_dataservices vendor/qcom/opensource/dataservices
+git clone -b android-10 https://github.com/aosp-stuff/platform_vendor_qcom_opensource_dataservices vendor/qcom/opensource/dataservices
 git clone -b lineage-17.1 https://github.com/LineageOS/android_vendor_qcom_opensource_data-ipa-cfg-mgr vendor/qcom/opensource/data-ipa-cfg-mgr
-git clone -b lineage-17.1 https://github.com/LineageOS/android_vendor_qcom_opensource_power vendor/qcom/opensource/power
+git clone -b android-10 https://github.com/aosp-stuff/platform_vendor_qcom_opensource_power vendor/qcom/opensource/power
 
 git clone -b android-10 https://github.com/aosp-stuff/device_asus_X00P device/asus/X00P
 git clone -b android-10 https://github.com/aosp-stuff/device_asus_X00P-kernel device/asus/X00P-kernel
